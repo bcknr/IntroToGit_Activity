b05cc128b0c069fca1ef35e246419bb0928e9699
bcknr <buckner.mark@icloud.com>
2023-01-31 16:11:09 -0500

    Basic script

---
+++ script.R
@@ -0,0 +1,15 @@
+# Load Iris dataset and ggplot2
+library(datasets)
+library(ggplot2)
+data("iris")
+
+# Plot
+plot <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point(size = 2) +
+  geom_smooth() +
+  xlab("Sepal Length") +
+  ylab("Sepal Width") +
+  scale_color_manual(values = c("tan", "wheat", "burlywood2")) +
+  theme_bw()
+  
+plot
