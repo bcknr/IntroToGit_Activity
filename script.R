d7f653d45ccb92e33f1e3593e008c835006c227f
bcknr <mab677@cornell.edu>
2023-01-31 17:27:07 -0500

    Added name comments

--- script.R
+++ script.R
@@ -3,6 +3,10 @@ library(datasets)
 library(ggplot2)
 data("iris")
 
+#Partner 1:
+
+#Partner 2: 
+
 # Plot
 plot <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
   geom_point(size = 2) +
