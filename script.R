3a6cc74ed03453b8e633630c84873ec8ca22f27f
bcknr <buckner.mark@icloud.com>
2023-01-31 17:24:29 -0500

    Added units to length

--- script.R
+++ script.R
@@ -7,7 +7,7 @@ data("iris")
 plot <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
   geom_point(size = 2) +
   geom_smooth() +
-  xlab("Sepal Length") +
+  xlab("Sepal Length (mm)") +
   ylab("Sepal Width") +
   scale_color_manual(values = c("olivedrab4", "aquamarine4", "burlywood2")) +
   theme_bw()
