586c5d495ae372b695a01ca456123ed4fbad6d17
bcknr <mab677@cornell.edu>
2023-01-31 17:24:39 -0500

    Added units to width

--- script.R
+++ script.R
@@ -8,7 +8,7 @@ plot <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
   geom_point(size = 2) +
   geom_smooth() +
   xlab("Sepal Length") +
-  ylab("Sepal Width") +
+  ylab("Sepal Width (mm)") +
   scale_color_manual(values = c("olivedrab4", "aquamarine4", "burlywood2")) +
   theme_bw()
 
